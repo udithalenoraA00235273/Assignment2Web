73b076317410a4f0d62bd66adc1b20dbc33a390e
udithalenoraA00235273 <A00235273@mycambrian.ca>
2021-11-19 17:46:13 -0500

    Setting up the test controller and new page

--- Assignment2Web/Controllers/HomeController.cs
+++ Assignment2Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Assignment2Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -23,6 +24,7 @@ namespace Assignment2Web.Controllers
             return View();
         }
 
+       
         public IActionResult Privacy()
         {
             return View();
@@ -32,6 +34,12 @@ namespace Assignment2Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
         }
     }
 }
